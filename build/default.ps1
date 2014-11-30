ff1c5b6e21f6bb0c36c3e55676f1bc573ae8d093
Edward Wilde <ewilde@gmail.com>
2014-11-30 21:01:53 +0000

    crane project build now runs all tests as part of the build

--- build/default.ps1
+++ build/default.ps1
@@ -10,12 +10,13 @@ param(
   $src_dir = "$build_dir\..\src"
   $sln_filename = "Crane.sln"
   $sln_filepath = "$src_dir\$sln_filename" 
- 
+  $xunit_consoleRunner = "$src_dir\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
+      
 
 Import-Module (Join-Path $build_dir 'psake-ext.psm1')
 FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
 
-Task Default -Depends BuildCrane
+Task Default -Depends BuildCrane, Test
 
 Task BuildCrane -Depends Info, Clean, Build
 
@@ -25,6 +26,7 @@ Task Info {
   Write-Host src_dir: $src_dir
   Write-Host sln_filename: $sln_filename
   Write-Host sln_filepath: $sln_filepath
+  Write-Host sln_filepath: $xunit_consoleRunner
 }
 
 Task Build -Depends Clean, NugetRestore { 
@@ -51,4 +53,11 @@ Task NugetRestore -Depends NugetExists {
 
 Task NugetExists { 
     Invoke-DownloadNuget $build_dir #doesn't download if exists
+}
+
+Task Test {
+    Get-ChildItem -Path $build_artifacts_dir -Filter *.Tests.dll | 
+    % {
+        & $xunit_consoleRunner @($_.FullName, '/silent')
+    }
 }
