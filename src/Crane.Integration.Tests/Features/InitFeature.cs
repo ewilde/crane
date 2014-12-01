5c7b11a47271771c29657dada989b3890f7508b3
Kevin Holditch <kevholditch@hotmail.com>
2014-12-01 17:18:02 +0000

    added integration test for 'crane init project'

--- src/Crane.Integration.Tests/Features/InitFeature.cs
+++ src/Crane.Integration.Tests/Features/InitFeature.cs
@@ -1,4 +1,5 @@
-﻿using Crane.Integration.Tests.TestUtilities;
+﻿using System.IO;
+using Crane.Integration.Tests.TestUtilities;
 using FluentAssertions;
 using Xbehave;
 
@@ -17,6 +18,26 @@ namespace Crane.Integration.Tests.Features
 
             "Then I receive the text did you mean 'crane init projectname'?"
                 ._(() => result.StandardOutput.Should().Be("did you mean 'crane init projectname'?"));
+        }
+
+        [Scenario]
+        public void Init_with_a_project_name_creates_a_project(Run run, RunResult result)
+        {
+            "Given I have crane in my path"
+                ._(() => run = new Run());
+
+            "When I run crane init ServiceStack"
+                ._(() => result = run.Command("crane init ServiceStack"));
+
+            "It should replace the solution file name in the build script with the project name"
+                ._(() => File.ReadAllText("./ServiceStack/build/default.ps1").Should().Contain("ServiceStack.sln"))
+                .Teardown(() =>
+                {
+                    Directory.Delete("./ServiceStack/build", recursive: true);
+                    Directory.Delete("./ServiceStack", recursive: true);
+                });
+
+
         }
 
         
