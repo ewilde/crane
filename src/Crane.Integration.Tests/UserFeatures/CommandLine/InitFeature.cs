45a7d4b74a38dbfe2307cf05e216d3d8207e61cd
Kevin Holditch <kevholditch@hotmail.com>
2014-12-03 14:53:04 +0000

    moved user level integration tests into folder

---
+++ src/Crane.Integration.Tests/UserFeatures/CommandLine/InitFeature.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Crane.Integration.Tests.TestUtilities;
+using FluentAssertions;
+using Xbehave;
+
+namespace Crane.Integration.Tests.UserFeatures.CommandLine
+{
+    public class InitFeature
+    {
+        [Scenario]
+        public void Init_with_no_arguments_returns_did_you_mean_init_projectname(Run run, RunResult result)
+        {
+            "Given I have crane in my path"
+                ._(() => run = new Run());
+
+            "When I run crane init"
+                ._(() => result = run.Command("crane init"));
+
+            "Then I receive the text did you mean 'crane init projectname'?"
+                ._(() => result.StandardOutput.Should().Be("did you mean 'crane init projectname'?"));
+        }
+
+        [Scenario]
+        public void Init_with_a_project_name_creates_a_project(Run run, RunResult result)
+        {
+            "Given I have crane in my path"
+                ._(() => run = new Run());
+
+            "When I run crane init ServiceStack"
+                ._(() => result = run.Command("crane init ServiceStack"));
+
+            "It should replace the solution file name in the build script with the project name"
+                ._(() => File.ReadAllText("./ServiceStack/build/default.ps1").Should().Contain("ServiceStack.sln"))
+                .Teardown(() =>  Directory.Delete("./ServiceStack", recursive: true));
+
+
+        }
+
+        
+    }
+}
