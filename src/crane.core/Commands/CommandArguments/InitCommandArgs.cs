4b43df1a3c7731668f4a3a909d7d42d693bf494b
Kevin Holditch <kevholditch@hotmail.com>
2014-12-08 07:41:15 +0000

    added command args for init command

---
+++ src/crane.core/Commands/CommandArguments/InitCommandArgs.cs
@@ -0,0 +1,11 @@
+﻿using PowerArgs;
+
+namespace Crane.Core.Commands.CommandArguments
+{
+    public class InitCommandArgs
+    {
+        [ArgPosition(0)]
+        [ArgRequired]
+        public string ProjectName { get; set; }
+    }
+}
