bb7af657ad8b293738690aff99ae86f133d417e2
Kevin Holditch <kevin.holditch@cityindex.co.uk>
2014-12-09 10:12:37 +0000

    neatened up code for creating console host

--- src/crane.core/Commands/Resolvers/CommandResolver.cs
+++ src/crane.core/Commands/Resolvers/CommandResolver.cs
@@ -6,13 +6,14 @@ namespace Crane.Core.Commands.Resolvers
 {
     public class CommandResolver : ICommandResolver
     {
+        private readonly Type _default = typeof (Help);
         public Type Resolve(IEnumerable<ICraneCommand> commands, string commandArgument)
         {
             
             var command = commands.FirstOrDefault(c => c.GetType().Name.ToLowerInvariant() == commandArgument.ToLowerInvariant());
 
             if (command == null)
-                return typeof (Help);
+                return _default;
 
             return command.GetType();
         }
