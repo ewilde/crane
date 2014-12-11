86bb34a5e8d2a16e9b31a2252972b0469f7d862d
Kevin Holditch <kevin.holditch@cityindex.co.uk>
2014-12-11 16:14:30 +0000

    Enabled ncrunch

---
+++ src/crane.core.tests/TestUtilities/EnableNCrunch.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace Crane.Core.Tests.TestUtilities
+{
+    public class EnableNCrunch
+    {
+        [Fact]
+        public void HelloNCrunch()
+        {
+        } 
+    }
+}
