705e5de43d9259b9991b215e644449c9ca03742e
Kevin Holditch <kevholditch@hotmail.com>
2015-01-07 20:35:16 +0000

    moved chocolatey tasks into module.  Trying to fix problem with version number patching

--- build/add-includes.ps1
+++ build/add-includes.ps1
@@ -6,3 +6,4 @@ Import-Module (Join-Path $build_dir "psake-ext.psm1") -Force
 Import-Module (Join-Path $build_dir "modules/buildtasks.psm1") -Force
 Import-Module (Join-Path $build_dir "modules/types.psm1") -Force
 Import-Module (Join-Path $build_dir "modules/generaltasks.psm1") -Force
+Import-Module (Join-Path $build_dir "modules/chocolateytasks.psm1") -Force
--- build/default.ps1
+++ build/default.ps1
@@ -8,15 +8,6 @@ properties{
 }
 
 $build_dir = (Split-Path $psake.build_script_file)
-$root_dir =  Resolve-Path "$build_dir\.."
-$build_artifacts_dir = "$root_dir\build-output"
-$src_dir = "$root_dir\src"
-$template_source_dir = "$src_dir\Crane.Templates"
-$sln_filename = "Crane.sln"
-$sln_filepath = "$src_dir\$sln_filename"
-$xunit_consoleRunner = "$src_dir\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
-$version = ""
-
 $add_includes = Join-Path $build_dir "add-includes.ps1"
 
 & $add_includes @($build_dir)
@@ -24,10 +15,8 @@ $add_includes = Join-Path $build_dir "add-includes.ps1"
 
 FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
 
-
 Task TeamCityBuildStep -Depends PatchAssemblyInfo, BuildCrane, Test, ChocolateyPublishPackage
 Task Default -Depends BuildCrane, Test
-
 Task BuildCrane -Depends SetupContext, Clean, Build
 
 
@@ -42,41 +31,9 @@ Task NugetExists {
 }
 
 
-Task ChocolateyExists{
-    try{
-	    & choco
-    }catch{
-        iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-    }
-}
-
-Task ChocolateyBuildPackage -Depends ChocolateyExists{
-    $choco_output_dir = "$($global:context.root_dir)\chocolatey-output"
-    $choco_nuspec = "$choco_output_dir\crane.nuspec"
-
-    Remove-Item $choco_output_dir -Recurse -Force -ErrorAction SilentlyContinue
-    New-Item -ItemType directory -Path $choco_output_dir -Force
-
-    $nuspectemplate = Get-Content "$($global:context.root_dir)\src\Crane.Chocolatey\crane.nuspec" | Out-String
-    $nuspectemplate = $nuspectemplate.Replace("##version_number##", $($global:context.build_version))
-    $nuspectemplate = $nuspectemplate.Replace("##build_output##", $($global:context.build_artifacts_dir))
-
-
-    New-Item -Path $choco_nuspec -ItemType File -Value $nuspectemplate
-    & cpack @($choco_nuspec)
-
-    Move-Item *.nupkg $choco_output_dir
-}
-
-Task ChocolateyPublishPackage -Depends ChocolateyBuildPackage{
-    Get-ChildItem "$($global:context.root_dir)\chocolatey-output" -Filter *.nupkg |
-    Foreach-Object{
-        & $build_dir\nuget.exe @('push', $_.FullName, "-s", $chocolateyApiUrl, $chocolateyApiKey)
-    }
-}
-
 Task PatchAssemblyInfo {
     $version = $global:context.build_version
+    Write-Host "version number is... $version"
     GenerateAssemblyInfo "Crane.Core" "Core crane functionality" $version "$src_dir\crane.core\Properties\AssemblyInfo.cs"
 
   	if ($teamcityBuild) {
