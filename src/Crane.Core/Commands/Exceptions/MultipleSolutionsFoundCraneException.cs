1cfd1561f9d90530c1b3b104670cc013fb436032
Kevin Holditch <kevholditch@hotmail.com>
2015-01-09 15:42:20 +0000

    Implemented resolver to get the path to the solution file relative to the build file

---
+++ src/Crane.Core/Commands/Exceptions/MultipleSolutionsFoundCraneException.cs
@@ -0,0 +1,11 @@
+﻿namespace Crane.Core.Commands.Exceptions
+{
+    public class MultipleSolutionsFoundCraneException : CraneException
+    {
+        public MultipleSolutionsFoundCraneException(params string [] solutionNames)
+            : base(string.Format("The path should only contain one solution please exlude the others using exclusion rules.  Solutions found {0}", string.Join(" ,", solutionNames)))
+        {
+            
+        }
+    }
+}
