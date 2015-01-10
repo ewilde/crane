1e3bed2b9b1545dd38fbc3f73731156e02f9d5f4
Kevin Holditch <kevholditch@hotmail.com>
2015-01-10 21:11:04 +0000

    Added a better help description for the assemble command

--- src/Crane.Core/Commands/Assemble.cs
+++ src/Crane.Core/Commands/Assemble.cs
@@ -9,6 +9,25 @@ namespace Crane.Core.Commands
     /// <example>
     /// example 1
     /// <code>crane assemble SallyFx</code>
+    /// 
+    /// This creates a new build inside the SallyFx folder.  The build scripts will be contained inside a build folder.
+    ///  
+    /// By default the build will perform the following steps
+    ///     - Patch assembly info - the assembly info of your assemblies with the version found in the version.txt 
+    ///     - Build the solution
+    ///     - Run your tests (if they are xunit tests at the moment only xunit is supported)
+    /// 
+    /// Once you have the build you can invoke by running
+    ///     .\build
+    /// 
+    /// To setup in TeamCity.
+    ///     - Create a new build
+    ///     - Add one build step to invoke a powershell script and paste in the following:
+    ///         ".\build.ps1 -tasklist @("TeamCityBuildStep") -build_number %build.number% -teamcity_build"
+    /// 
+    /// That's it! You will now have a fully running CI build on TeamCity.  
+
+    /// 
     /// </example>
     public class Assemble : ICraneCommand
     {
