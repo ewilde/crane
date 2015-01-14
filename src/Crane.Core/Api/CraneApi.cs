b2f752a38850ca7d3964411d64505516feb12cc8
Kevin Holditch <kevholditch@hotmail.com>
2015-01-14 12:57:23 +0000

    Starting to flesh out the builder api

---
+++ src/Crane.Core/Api/CraneApi.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crane.Core.Api
+{
+    public static class CraneApi
+    {
+        public static SolutionContext GetSolutionContext(string rootFolderPath)
+        {
+            return new SolutionContext();
+        }
+
+        public static SolutionBuilder CreateSolution(string rootFolderPath)
+        {
+            return new SolutionBuilder(rootFolderPath);
+        }
+        
+    }
+
+    public class SolutionContext
+    {
+        public IEnumerable<Project> Projects { get; set; }
+    }
+
+    public class SolutionBuilder
+    {
+        private string _rootPath;        
+        private List<Project> _projects; 
+
+        public SolutionBuilder(string rootPath)
+        {
+            _rootPath = rootPath;            
+            _projects = new List<Project>();
+        }
+
+
+        public SolutionBuilder WithProject(Action<Project> assign)
+        {
+            var p = new Project();
+            assign(p);
+            _projects.Add(p);
+            return this;
+        }
+
+        public SolutionContext Build()
+        {
+            return null;
+        }
+    }
+
+    public class ProjectBuilder
+    {
+        private Project _project;
+
+        public ProjectBuilder()
+        {
+            _project = new Project();
+        }
+
+
+        public ProjectBuilder WithName(string name)
+        {
+            _project.Name = name;
+            return this;
+        }
+
+        public Project Build()
+        {
+            return null;
+        }
+    }
+
+    public class Project
+    {
+        public string Name { get;  set; }    
+    }
+
+    public class Test
+    {
+        public void Foo()
+        {
+            var solutionContext =
+                CraneApi.CreateSolution("path")
+                    .WithProject(p => p.Name = "SallyFx")
+                    .WithProject(p => p.Name = "SallyFx.Common").Build();
+
+            
+        }
+    }
+}
---
+++ src/Crane.Integration.Tests/Features/Api/CraneApiFeatures.cs
@@ -0,0 +1,13 @@
+﻿using Xbehave;
+
+namespace Crane.Integration.Tests.Features.Api
+{
+    public class CraneApiFeatures
+    {
+        [Scenario]
+        public void Get_context_with_root_folder_path_returns_all_projects()
+        {
+            
+        }
+    }
+}
