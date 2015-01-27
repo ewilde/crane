70af1cb1476e6c19a7904b3c971547e3d9688ae8
Edward Wilde <ewilde@gmail.com>
2015-01-27 06:15:58 +0000

    intermediate

---
+++ src/Crane.Core/Api/PowerShell/CraneCmdlet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Management.Automation;
+using log4net;
+using Crane.Core.Extensions;
+namespace Crane.Core.Api.PowerShell
+{
+    internal abstract class CraneCmdlet : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                this.Initialize();
+                Process();
+            }
+            catch (Exception exception)
+            {
+                this.Log.ErrorFormat("Error processing command. Error: {0}", exception.ToString());
+                throw;
+            }
+        }
+
+        protected abstract ILog Log { get; }
+
+        protected virtual void Initialize()
+        {
+        }
+
+        internal abstract void Process();
+
+        internal static bool RunningInPowerShell
+        {
+            get { return System.Diagnostics.Process.GetCurrentProcess()
+                    .ProcessName.Contains("powershell", StringComparison.InvariantCultureIgnoreCase); }
+        }
+    }
+}
